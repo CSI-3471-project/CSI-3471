11eb6d14e502b21da46d04461d3695e0927f7011
TinaXiayanLi <Xiayan_Li2@baylor.edu>
2020-10-28 19:49:58 -0500

    not save the earlier version

--- iterationII/src/main/java/project/iterationII/SellItemPostWindow.java
+++ iterationII/src/main/java/project/iterationII/SellItemPostWindow.java
@@ -1,14 +1,57 @@
 package project.iterationII;
 
 import javax.swing.JPanel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.Font;
+import javax.swing.JTextField;
 
 public class SellItemPostWindow extends JPanel {
+	private JTextField textField;
 
 	/**
 	 * Create the panel.
 	 */
 	public SellItemPostWindow() {
+		
+		JButton btnNewButton = new JButton("New button");
+		
+		JLabel lblNewLabel = new JLabel("Name\r\n");
+		lblNewLabel.setFont(new Font("Calibri", Font.PLAIN, 26));
+		
+		textField = new JTextField();
+		textField.setFont(new Font("Calibri", Font.PLAIN, 26));
+		textField.setColumns(10);
+		GroupLayout groupLayout = new GroupLayout(this);
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(146)
+							.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 134, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(122)
+							.addComponent(lblNewLabel)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(textField, GroupLayout.PREFERRED_SIZE, 123, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap(132, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(68)
+					.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 135, GroupLayout.PREFERRED_SIZE)
+					.addGap(7)
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addComponent(lblNewLabel)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(272, Short.MAX_VALUE))
+		);
+		setLayout(groupLayout);
 
 	}
-
 }
