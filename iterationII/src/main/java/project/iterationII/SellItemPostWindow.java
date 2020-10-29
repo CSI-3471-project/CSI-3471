42b12750381d619147d605c87673bde04e966b5e
TingChenWang <60168970+emilywang2020@users.noreply.github.com>
2020-10-29 01:17:15 -0500

    add calendar & modified files

--- iterationII/src/main/java/project/iterationII/SellItemPostWindow.java
+++ iterationII/src/main/java/project/iterationII/SellItemPostWindow.java
@@ -20,23 +20,23 @@ public class SellItemPostWindow extends JPanel {
 	 */
 	public SellItemPostWindow() {
 		
-		JButton btnNewButton = new JButton("New button");
+		JButton btnNewButton = new JButton("view image:");
 		
-		JLabel lblNewLabel = new JLabel("Name\r\n");
+		JLabel lblNewLabel = new JLabel("Name:\r\n");
 		lblNewLabel.setFont(new Font("Calibri", Font.PLAIN, 26));
 		
 		textField = new JTextField();
 		textField.setFont(new Font("Calibri", Font.PLAIN, 26));
 		textField.setColumns(10);
 		
-		JLabel lblPrice = new JLabel("Price");
+		JLabel lblPrice = new JLabel("Price:");
 		lblPrice.setFont(new Font("Calibri", Font.PLAIN, 26));
 		
 		JFormattedTextField formattedTextField = new JFormattedTextField();
 		formattedTextField.setText("$");
 		formattedTextField.setFont(new Font("Calibri", Font.PLAIN, 26));
 		
-		JLabel lblNewLabel_1 = new JLabel("Description\r\n");
+		JLabel lblNewLabel_1 = new JLabel("Description:\r\n");
 		lblNewLabel_1.setFont(new Font("Calibri", Font.PLAIN, 26));
 		
 		JTextArea textArea = new JTextArea();
@@ -47,43 +47,37 @@ public class SellItemPostWindow extends JPanel {
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(128)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(lblPrice, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(formattedTextField))
+							.addGap(177)
+							.addComponent(btnNewButton_1, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE))
 						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(lblNewLabel)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(textField, GroupLayout.PREFERRED_SIZE, 123, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap(132, Short.MAX_VALUE))
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(158)
-					.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 134, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(159, Short.MAX_VALUE))
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(162)
-					.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 120, Short.MAX_VALUE)
-					.addGap(278))
-				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
-					.addContainerGap(61, Short.MAX_VALUE)
-					.addComponent(textArea, GroupLayout.PREFERRED_SIZE, 328, GroupLayout.PREFERRED_SIZE)
-					.addGap(171))
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(177)
-					.addComponent(btnNewButton_1, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(273, Short.MAX_VALUE))
+							.addGap(54)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(textArea, GroupLayout.PREFERRED_SIZE, 341, GroupLayout.PREFERRED_SIZE)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+										.addComponent(lblNewLabel)
+										.addComponent(lblPrice, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE))
+									.addGap(18)
+									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+										.addComponent(formattedTextField)
+										.addComponent(textField, GroupLayout.DEFAULT_SIZE, 133, Short.MAX_VALUE)))
+								.addComponent(lblNewLabel_1)))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(158)
+							.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 134, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap(56, Short.MAX_VALUE))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addGap(39)
-					.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 135, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 127, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(textField, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNewLabel))
+						.addComponent(lblNewLabel)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblPrice, GroupLayout.PREFERRED_SIZE, 33, GroupLayout.PREFERRED_SIZE)
