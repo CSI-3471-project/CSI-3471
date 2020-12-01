812544dc45ab3b4f62c528785765dc42f506bbda
TinaXiayanLi <Xiayan_Li2@baylor.edu>
2020-12-01 00:01:38 -0600

    add postInfoPanel, RequestPostInfoPanel and SellItemPostInfoPanel

---
+++ iterationIII/iterationIII/src/main/java/GUI/PostInfoPanel.java
@@ -0,0 +1,14 @@
+package GUI;
+
+import javax.swing.JPanel;
+
+public class PostInfoPanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public PostInfoPanel() {
+
+	}
+
+}
---
+++ iterationIII/iterationIII/src/main/java/GUI/SellPostInfoPanel.java
@@ -0,0 +1,14 @@
+package GUI;
+
+import javax.swing.JPanel;
+
+public class SellPostInfoPanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public SellPostInfoPanel() {
+
+	}
+
+}
